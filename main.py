38d0b9923de21c77f9c05fd303928d9cdc7622e5
Jacob Kipchirchir <jaycjacob@gmail.com>
2021-08-17 19:26:49 +0300

    save results

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+import pandas as pd
 
 driver = webdriver.Chrome("c:\\chromedriver.exe")
 
@@ -11,12 +12,15 @@ url = 'https://www.google.com/maps/search/' + starting_bs + ' ' + country
 
 driver.get(url)
 
-WebDriverWait(driver, 25)
-
+WebDriverWait(driver, 30)
+time.sleep(30)
 d = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/span[1]/span[1]/button')
 d.click()
 WebDriverWait(driver, 25)
 time.sleep(30)
 bs_names= [i.text for i in driver.find_elements_by_css_selector('[class="qBF1Pd gm2-subtitle-alt-1"]')]
+
+ps = pd.DataFrame(data={'names':bs_names})
+ps.to_csv('names.cvs', index = False)
 print(bs_names)
 driver.close()
