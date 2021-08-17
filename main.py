564db39b7e9c66f8d6fff618ae68508f04c9d517
Jacob Kipchirchir <jaycjacob@gmail.com>
2021-08-17 15:08:12 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome("c:\\chromedriver.exe")
+
+starting_bs = 'The carnivore restaurant'
+country = 'kenya'
+url = 'https://www.google.com/maps/search/' + starting_bs + ' ' + country
+
+driver.get(url)
+
+WebDriverWait(driver, 25)
+
+d = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/span[1]/span[1]/button')
+d.click()
+WebDriverWait(driver, 25)
+time.sleep(30)
+bs_names= [i.text for i in driver.find_elements_by_css_selector('[class="qBF1Pd gm2-subtitle-alt-1"]')]
+print(bs_names)
+driver.close()
