78c0f004a6e708358808859b6e37fe485c70cd03
Jacob Kipchirchir <jaycjacob@gmail.com>
2021-08-20 16:32:02 +0300

    scrape address

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ driver = webdriver.Chrome("c:\\chromedriver.exe")
 starting_bs = 'The carnivore restaurant'
 country = 'kenya'
 bs_names = []
+adresses = []
 url = 'https://www.google.com/maps/search/' + starting_bs + ' ' + country
 
 driver.get(url)
@@ -25,12 +26,12 @@ d = driver.find_element_by_xpath(
 d.click()
 for i in tqdm(range(3), leave=False, desc='1. 1st round'):
     print(bs_names)
+    print(adresses)
     time.sleep(30)
  #   WebDriverWait(driver, 30)
 #    d = driver.find_elements_by_xpath('//div[@class="qBF1Pd gm2-subtitle-alt-1"]')
  #   #print("jjjj", len(d))
     for r in range(6):
-        print("dada")
         driver.execute_script("arguments[0].scrollIntoView();", driver.find_elements_by_xpath('//div[@class="qBF1Pd gm2-subtitle-alt-1"]')[-1])
         time.sleep(5)
 #    touch_actions = TouchActions(driver)
@@ -38,7 +39,11 @@ for i in tqdm(range(3), leave=False, desc='1. 1st round'):
  #   time.sleep(15)
 
     bs = [i.text for i in driver.find_elements_by_css_selector('[class="qBF1Pd gm2-subtitle-alt-1"]')]
+    ad = [i.text for i in driver.find_elements_by_css_selector('[class="ZY2y6b-RWgCYc"]')]
+    ab = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[4]/div[1]/div[1]/div/div[2]/div[2]/div[1]/div/div/div/div[4]/div[1]/span[2]/jsl')
+    print("ababa", ab.text)
     bs_names += bs
+    adresses += ad
 
     try:
         WebDriverWait(driver, 40).until(
